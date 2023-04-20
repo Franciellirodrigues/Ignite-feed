e2e23d52c8aac0e150184f2fd22e7aa765426a1a
Francielli Rodrigues <contato.franciellirodrigues@outlook.com>
2023-04-20 12:06:27 -0300

    feat: Comment and Comment.module. feat: add css file varibles and styles Post.modules

---
+++ src/components/Comment.jsx
@@ -0,0 +1,45 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://images.unsplash.com/photo-1601455763557-db1bea8a9a5a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=50" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time title="11 de maio às 08:13h" dateTime="2023-04-20">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏🏻👏🏻</p>
+
+
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+
+
+      </div>
+
+
+
+
+
+
+
+    </div>
+  )
+}
