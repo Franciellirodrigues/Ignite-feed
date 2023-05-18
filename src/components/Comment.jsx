a9839f574ced71b2f8991ad72ef9ecd0bea64ff7
Francielli Rodrigues <contato.franciellirodrigues@outlook.com>
2023-05-18 11:34:15 -0300

    Comunicação entre componentes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,13 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    console.log('deletar')
+
+    onDeleteComment(content);
+
+  }
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://images.unsplash.com/photo-1601455763557-db1bea8a9a5a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=50" alt="" />
@@ -15,7 +21,7 @@ export function Comment({ content }) {
               <time title="11 de maio às 08:13h" dateTime="2023-04-20">Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
