f43519223ce24048c4fed56c0393f56c703a8a5c
Francielli Rodrigues <contato.franciellirodrigues@outlook.com>
2023-05-18 13:50:34 -0300

    Validando formulário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,7 +34,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
-    setNewCommentText(event.target.value)
+    event.target.setCustomValidity('');
+    setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório!');
   }
 
   function deleteComment(commentToDelete) {
@@ -45,6 +50,7 @@ export function Post({ author, publishedAt, content }) {
     setComments(commentsWithoutDeletedOne)
   }
 
+  const isNewCommentEmpty = newCommentText.length == 0;
 
   return (
     <article className={styles.post}>
@@ -80,10 +86,14 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}
+          >Publicar
+          </button>
         </footer>
       </form>
 
